07358ff65f5dd186292a3fc5c2112e1be26e8076
Savyasachee Jha <genghizkhan91@hawkradius.com>
2020-05-29 11:31:05 +0530

    Small changes to the site config

--- siteConfig.js
+++ siteConfig.js
@@ -1,23 +1,23 @@
 module.exports = {
     // Do not include a trailing slash!
-    siteUrl: `https://www.jamify.org`,
+    siteUrl: `https://hawkradius.com`,
 
     // Maximum number of post shown per page
     // With Infinite Scroll this is the chunk of posts fetched when scrolling down
-    postsPerPage: 3,
+    postsPerPage: 5,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `Hawkradius`,
 
     // This allows an site description for meta data for pages.
-    siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
+    siteDescriptionMeta: `A blog about history, current affairs, policy and science`,
 
     // Change to the width and height of your default share image
     shareImageWidth: 1000,
     shareImageHeight: 523,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `Hawkradius`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
