032a235d6c42c9904c3a41b0b8956f5cbbbf8932
Savyasachee Jha <genghizkhan91@hawkradius.com>
2020-06-14 13:07:05 +0530

    Re-enabled infinite scroll

--- siteConfig.js
+++ siteConfig.js
@@ -2,6 +2,9 @@ module.exports = {
     // Do not include a trailing slash!
     siteUrl: `https://hawkradius.com`,
 
+    // Enable infinite scroll (default: true)
+    infiniteScroll: true,
+
     // Maximum number of post shown per page
     // With Infinite Scroll this is the chunk of posts fetched when scrolling down
     postsPerPage: 5,
@@ -17,7 +20,7 @@ module.exports = {
     shareImageHeight: 523,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Hawkradius`,
+    shortTitle: `Hawk Radius`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
